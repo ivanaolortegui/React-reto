adf99f83ffa84ac4e7b1c42bd0f4470c5ec1296c
ivanaolortegui <olorteguimor@gmail.com>
2019-09-10 12:54:59 -0500

    Updates

---
+++ precache-manifest.c267df333e813b96e3fa8c80ed79cc8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2613566f2e51177481fb644f7f469f20",
+    "url": "/React-reto/index.html"
+  },
+  {
+    "revision": "0b3ee04e03c7ae163f5b",
+    "url": "/React-reto/static/css/main.b8bd81b4.chunk.css"
+  },
+  {
+    "revision": "ca849228dd2d4b10670b",
+    "url": "/React-reto/static/js/2.cd237ee8.chunk.js"
+  },
+  {
+    "revision": "0b3ee04e03c7ae163f5b",
+    "url": "/React-reto/static/js/main.a3efc7e7.chunk.js"
+  },
+  {
+    "revision": "c13bb4d8c1f3f68a4cf9",
+    "url": "/React-reto/static/js/runtime~main.fc7bd6a0.js"
+  }
+]);
